7c8a3d9f1de8bff38ff6eeafa33f0616574bb4f8
JoaoClaudioLima <j.claudiobl@gmail.com>
2021-03-09 14:53:28 -0300

    Criação do Controle de Database
    Criação das conexões com a Database

---
+++ src/Controller/DatabaseController.java
@@ -0,0 +1,35 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseController {
+    public static Connection preConnect(){
+        Connection connect = null;
+        try {
+            String url = "jdbc:mysql://localhost";
+            String user = "root";
+            String password = "";
+            connect = DriverManager.getConnection(url,user,password);
+            System.out.println("Connected");
+        }catch (Exception error){
+            System.out.println(error.getMessage());
+        }
+        return connect;
+    }
+
+    public static Connection connect(){
+        Connection connect = null;
+
+        try {
+            String url = "jdbc:mysql://localhost/event";
+            String user = "root";
+            String password = "";
+            connect = DriverManager.getConnection(url,user,password);
+            System.out.println("Connected");
+        }catch (Exception error){
+            System.out.println(error.getMessage());
+        }
+        return connect;
+    }
+}
