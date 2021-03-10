28a705b366da293c6e62fb6b4fe9dd0e8b1f2a28
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-03-09 22:50:55 -0300

    Sortear pessoas nas salas
    Feito o controller para definir as salas e espaços de cada pessoa.

---
+++ src/Controller/UsersController.java
@@ -0,0 +1,63 @@
+package Controller;
+
+import Model.RoomsModel;
+
+import java.util.ArrayList;
+
+public class UsersController {
+    public static Object userRaffle(ArrayList<String> users, String[][] rooms, ArrayList<String> coffes) {
+
+        int room = 0;
+        int room2 = 0;
+        int coffe = 0;
+        int coffe2 = 0;
+        String[][]SelectedPeople = new String[users.size()][5];
+
+        for(int i=0; i < users.size(); i++) {
+            if(room >= rooms.length) {
+                room = 0;
+            }
+
+            SelectedPeople[i][0] = users.get(i);
+            SelectedPeople[i][1] = String.valueOf(room);
+
+
+            room++;
+        }
+
+        for(int i=0; i < users.size(); i++) {
+            coffe = (Integer.parseInt(SelectedPeople[i][1]) + 1);
+            if(coffe >= coffes.size()) {
+                coffe = 0;
+            }
+
+            SelectedPeople[i][3] = String.valueOf(coffe);
+
+            coffe++;
+        }
+
+        for(int i=0; i < users.size(); i++) {
+            room2 = (Integer.parseInt(SelectedPeople[i][1]) + 1);
+            if(room2 >= rooms.length) {
+                room2 = 0;
+            }
+
+            SelectedPeople[i][2] = String.valueOf(room2);
+            room2++;
+        }
+
+        for(int i=0; i < users.size(); i++) {
+            coffe2 = (Integer.parseInt(SelectedPeople[i][2]) + 1);
+            if(coffe2 >= coffes.size()) {
+                coffe2 = 0;
+            }
+
+            SelectedPeople[i][4] = String.valueOf(coffe2);
+            System.out.println("Nome: " + SelectedPeople[i][0] + " \t|\t Primeira Sala: " + SelectedPeople[i][1] + "\t|\tPrimeiro Espaço: " + SelectedPeople[i][3] + "\t|\tSegunda Sala: " + SelectedPeople[i][2] + "\t|\tSegundo Café: " + SelectedPeople[i][4]);
+            coffe2++;
+        }
+
+
+        return SelectedPeople;
+    }
+}
