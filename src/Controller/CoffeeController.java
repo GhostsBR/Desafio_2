a201e63e30c22eafada33bfbc7f8fd9c6e6da0e8
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-03-11 12:17:01 -0300

    Criado o Controlador dos Espaços
    Criado o controlador para os espaços.

---
+++ src/Controller/CoffeeController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import Model.Coffee;
+import Model.CoffeeDAO;
+
+import java.util.List;
+
+/**
+ * Classe responsável pelo controle dos espaços.
+ *
+ * @author Gustavo Lemos
+ */
+public class CoffeeController {
+    /**
+     * Método responsável por separar e enviar os espaços para serem inseridos no banco de dados.
+     *
+     * Apôs o recebimento de uma lista de objetos, o método separa as salas
+     * e realiza o envio ao método responsável por inserir no banco de dados.
+     *
+     * @param coffees List<Coffee> Lista de objetos de espaços.
+     */
+    public static void insertCoffee(List<Coffee> coffees) {
+        for (int i=0; i < coffees.size(); i++) {
+            CoffeeDAO cd = new CoffeeDAO();
+            cd.createCoffee(coffees.get(i));
+        }
+    }
+}
