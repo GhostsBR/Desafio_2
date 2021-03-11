ea75dec91cbcae606052538f6474b86a50c08a53
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-03-11 12:16:19 -0300

    Criado o Controlador de Salas
    Criado o controlador das salas.

---
+++ src/Controller/RoomController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import Model.Room;
+import Model.RoomDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe responsável pelo controle de salas.
+ *
+ * @author Gustavo Lemos
+ */
+public class RoomController {
+    /**
+     * Método responsável por separar e enviar as salas para serem inseridas no banco de dados.
+     *
+     * Apôs o recebimento de uma lista de objetos, o método separa as salas
+     * e realiza o envio ao método responsável por inserir no banco de dados.
+     *
+     * @param rooms List<Room> Lista de objetos de salas.
+     */
+    public static void insertRoom(List<Room> rooms) {
+        for(int i=0; i < rooms.size(); i++) {
+            RoomDAO.createRoom(rooms.get(i));
+        }
+    }
+}
