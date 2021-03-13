c0edb006dac66672472003b6f70469122bfcd776
Rafael Barros <rafael.eng.mecanic@gmail.com>
2021-03-13 00:31:07 -0300

    atualização da UserView

--- src/View/UserView.java
+++ src/View/UserView.java
@@ -5,19 +5,29 @@
  */
 package View;
 
+import Controller.RoomController;
+import CustomExceptions.CustomException;
+
 /**
  *
  * @author Administrador
  */
 public class UserView extends javax.swing.JFrame {
-
+ 
     /**
      * Creates new form Home
      */
     public UserView() {
         initComponents();
     }
-
+    
+    private void buscarSalas() {
+    	 try {
+			RoomController.findRooms();
+		} catch (CustomException e) {
+			e.printStackTrace();
+		}
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
