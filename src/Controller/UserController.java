255339cf263d9e06cc6777e65dffaf18113f53d3
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-03-16 17:11:37 -0300

    Modificado UserController
    Fix mensagem de erro.
    
    Removido espaços desnecessários.

--- src/Controller/UserController.java
+++ src/Controller/UserController.java
@@ -86,7 +86,6 @@ public class UserController {
             if(room >= rooms.size()) {
                 room = 0;
             }
-
             if(rooms.get(room).getCapacityRoom() == smallestRoom) {
                 if((rooms.get(room).getQuantity1() + 1) > (smallestRoom)) {
                     room = verifyBetterRoom(rooms, 1, smallestRoom);
@@ -96,7 +95,6 @@ public class UserController {
                     room = verifyBetterRoom(rooms, 1, smallestRoom);
                 }
             }
-
             if(room >= rooms.size()) {
                 room = 0;
             }
@@ -110,8 +108,6 @@ public class UserController {
             users.get(i).setIdTemp(i);
             rooms.get(room).setUsers(users.get(i));
             rooms.get(room).setQuantity1(rooms.get(room).getQuantity1() + 1);
-
-
             room++;
         }
 
@@ -130,15 +126,11 @@ public class UserController {
         }
 
         for (int i=0; i < rooms.size(); i++) {
-
             room2++;
-
             for (int n=0; n < rooms.get(i).getUsers().size(); n++) {
-
                 if(room2 >= rooms.size()) {
                     room2 = 0;
                 }
-
                 if(rooms.get(room2).getCapacityRoom() == smallestRoom) {
                     if((rooms.get(room2).getQuantity2() + 1) > (smallestRoom)) {
                         room2 = verifyBetterRoom(rooms,2, smallestRoom);
@@ -148,19 +140,15 @@ public class UserController {
                         room2 = verifyBetterRoom(rooms, 2, smallestRoom);
                     }
                 }
-
                 if(room2 >= rooms.size()) {
                     room2 = 0;
                 }
-
                 rooms.get(room2).setQuantity2(rooms.get(room2).getQuantity2() + 1);
-
                 try {
                     users.get(rooms.get(i).getUsers().get(n).getIdTemp()).setRoom2User(rooms.get(room2));
                 } catch (CustomException error) {
-                    System.out.println("Erro ao definir o número de usuários na sala 2: " + error.getMessage());
+                    System.out.println("Erro ao definir a segunda sala do usuário: " + error.getMessage());
                 }
-
                 room2++;
             }
         }
